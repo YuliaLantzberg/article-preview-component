fa962b70c00c7d6bd099dfc0a27cd60c201aba4a
YuliaLantzberg <lanzyuka@gmail.com>
2024-09-06 04:05:13 +0300

    Add hover effect for desktop. Fix bug - max-width = original width of the image

--- script.js
+++ script.js
@@ -8,11 +8,23 @@ const artl__footer = document.querySelector(".article__footer--regular");
 const socials = document.querySelector(".article__footer--active");
 
 share_btn.addEventListener("click", () => {
-	artl__footer.style.display = "none";
-	socials.style.display = "flex";
+	artl__footer.classList.remove("active");
+	socials.classList.add("active");
 });
 
 share_btn_socials.addEventListener("click", () => {
-	socials.style.display = "none";
-	artl__footer.style.display = "flex";
+	socials.style.classList.remove("active");
+	artl__footer.classList.add("active");
+});
+
+// for desktop
+const triangle = document.querySelector(".triangle-down");
+share_btn.addEventListener("mouseenter", () => {
+	socials.classList.add("hover");
+	triangle.classList.add("hover");
+});
+
+share_btn.addEventListener("mouseleave", () => {
+	socials.classList.remove("hover");
+	triangle.classList.remove("hover");
 });
